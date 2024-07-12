52e894c07d0f340974cafc71a34506d120825ec8
Camila Amaya <86732477+CmilAmaya@users.noreply.github.com>
2024-07-11 19:58:43 -0500

    Style correction

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 let input = document.querySelector('.text__area__input');
 
-function input_dissapear (input) {  
-    input.value = ''; 
-}
 
 function encrypt (input) {
-    let output = input.value; 
+    let output = input.value.trim();
 
-    output = output.replace(/e/g, 'enter');
-    output = output.replace(/i/g, 'imes');
-    output = output.replace(/a/g, 'ai');
-    output = output.replace(/o/g, 'ober');
-    output = output.replace(/u/g, 'ufat');
-    console.log(output)
+    if (output == "" ){
+        document.querySelector('.text__area__output__hide').style.display = 'none';
+        document.querySelector('.text__area__output__show').style.display = 'block';
+    } else {
+        output = output.replace(/e/g, 'enter');
+        output = output.replace(/i/g, 'imes');
+        output = output.replace(/a/g, 'ai');
+        output = output.replace(/o/g, 'ober');
+        output = output.replace(/u/g, 'ufat');
 
-    document.querySelector('.text__area__output__hide').style.display = 'block';
-    document.querySelector('.text__area__output__hide__p').innerHTML = output;
-
+        document.querySelector('.text__area__output__hide').style.display = 'block';
+        document.querySelector('.text__area__output__show').style.display = 'none'; 
+        document.querySelector('.text__area__output__hide__p').innerHTML = output;
+        document.querySelector('.text__area__output__hide__button').innerHTML = 'Copy';
+    }
 }
 
 function decrypt (input) {
-    let output = input.value; 
+    let output = input.value.trim();
+    
+    if (output == "") {
+        document.querySelector('.text__area__output__hide').style.display = 'none';
+        document.querySelector('.text__area__output__show').style.display = 'block';
+    } else {
+        output = output.replace(/enter/g, "e");
+        output = output.replace(/imes/g, "i");
+        output = output.replace(/ai/g, "a");
+        output = output.replace(/ober/g, "o");
+        output = output.replace(/ufat/g, "u");
 
-    output = output.replace(/enter/g, "e");
-    output = output.replace(/imes/g, "i");
-    output = output.replace(/ai/g, "a");
-    output = output.replace(/ober/g, "o");
-    output = output.replace(/ufat/g, "u");
-    console.log(output)
+        document.querySelector('.text__area__output__hide').style.display = 'block';
+        document.querySelector('.text__area__output__show').style.display = 'none';
+        document.querySelector('.text__area__output__hide__p').innerHTML = output;
+        document.querySelector('.text__area__output__hide__button').innerHTML = 'Copy';
+    }
 
-    document.querySelector('.text__area__output__hide').style.display = 'block';
-    document.querySelector('.text__area__output__hide__p').innerHTML = output;
+    
 
+}
+
+function copy() {
+    let copyText = document.querySelector('.text__area__output__hide__p').innerHTML;
+    console.log(copyText);
+    navigator.clipboard.writeText(copyText).then(function (){
+        document.querySelector('.text__area__output__hide__button').innerHTML = 'Copied!';
+    }).catch(function(err){
+        console.error('Could not copy text', err);
+    });
 }
 
